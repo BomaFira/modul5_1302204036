1521898053417757d3b8f902a7097002e0bc88c2
BomaFira <Cluezee@student.telkomuniversity.ac.id>
2022-03-26 16:30:00 +0700

    Add project files.

---
+++ modul5_1302204036/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics.Contracts;
+using modul5_1302204036;
+
+namespace modul5_1302204036
+{
+    internal class Program
+    {
+        public class SayaTubeUser
+        {
+            private int id;
+            private List<SayaTubeVideo> uploadedVideos;
+            private string Username;
+            private List<SayaTubeVideo> uploadedVideo;
+
+            public SayaTubeUser(string username)
+            {
+                Random shuffle = new Random();
+                id = shuffle.Next(0, 100000);
+                uploadedVideos = new List<SayaTubeVideo>();
+            }
+
+            public int GetTotalVideoPlayCount()
+            {
+                int total = 0;
+                foreach (SayaTubeVideo video in uploadedVideos)
+                {
+                    total += video.GetPlaycount();
+                }
+                return total;
+            }
+
+            public void AddVideo(SayaTubeVideo video)
+            {
+                uploadedVideos.Add(video);
+
+            }
+
+            public void PrintAllVideoPlaycount()
+            {
+                Console.WriteLine("User : " + this.AddVideo);
+                for (int i = 0; i < uploadedVideos.Count; i++)
+                {
+                    Console.WriteLine("Video " + (i + 1) + " Judul : " + uploadedVideos[i].GetTitle());
+                }
+            }
+        }
+    }
+}
+
